a991efd92260e941082c1a99d36cb6fc62cda9fa
pablocattalin <pablo.cattalin@gmail.com>
2022-08-10 14:33:08 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,9 @@ export class AppComponent {
             fieldGroupClassName: 'row',
             fieldGroup: [
               {
-                className: 'col col-lg-2',
-                type: 'checkbox',
                 key: 'consumidorFinal',
+                type: 'checkbox',
+                className: 'col col-lg-2',
                 templateOptions: {
                   label: 'Consumidor Final',
                 },
@@ -34,8 +34,7 @@ export class AppComponent {
                 className: 'col col-lg-4',
                 templateOptions: {
                   label: 'Nombre o Razón Social',
-                  required: true,
-                  appearance: 'outline',
+                  // required: true,
                 },
                 expressionProperties: {
                   'templateOptions.disabled': 'model.consumidorFinal',
@@ -53,29 +52,44 @@ export class AppComponent {
                   enableWorkCount: true,
                 }
               },
-              {
-                key: 'tipoCbteId',
-                type: 'select',
-                className: 'col col-lg-4',
-                templateOptions: {
-                  label: 'Tipo de Comprobante',
-                  minLength: 7,
-                  maxLength: 11,
-                  enableWorkCount: true,
-                },
-              },
-            ],
+            ]
           },
+          { template: '<hr />' },
           {
             fieldGroupClassName: 'row',
             fieldGroup: [
+              {
+                key: 'cbteTipoId',
+                type: 'select',
+                className: 'col col-lg-5',
+                templateOptions: {
+                  label: 'Tipo de Comprobante',
+                  required: true,
+                  options: [
+                    {label: 'FACTURA A', value: '1'},
+                    {label: 'NOTA DE DEBITO ELECTRÓNICA MiPyMEs (FCE) A', value: '202'},
+                    {label: 'NOTA DE CREDITO ELECTRÓNICA MiPyMEs (FCE) A', value: '203'}
+                  ]
+                },
+              },
+              {
+                key: 'anulaCbteAsociado',
+                type: 'checkbox',
+                className: 'col col-lg-3',
+                templateOptions: {
+                  label: 'Anula comprobante asociado',
+                },
+                expressionProperties: {
+                  hideExpression: 'model.cbteTipoId === "1"',
+                },
+              },
               {
                 key: 'puntoVenta',
                 type: 'select',
                 className: 'col col-lg-2',
                 templateOptions: {
                   label: 'Punto de Venta',
-                  appearance: 'outline',
+                  required: true,
                 },
               },
               {
@@ -85,19 +99,127 @@ export class AppComponent {
                 templateOptions: {
                   label: 'Fecha',
                   type: 'date',
+                  required: true,
                 },
               },
             ],
           },
           {
             fieldGroupClassName: 'row',
+            fieldGroup: [             
+              {
+                key: 'monedaId',
+                type: 'select',
+                className: 'col col-lg-2',
+                templateOptions: {
+                  label: 'Moneda',
+                  required: true,
+                  options: [
+                    { label: 'PESO ARG', value: 'PES' },
+                    { label: 'DOLAR', value: 'DOL' },
+                  ],
+                },
+              },
+              {
+                key: 'cotizacion',
+                type: 'input',
+                className: 'col col-lg-1',
+                defaultValue: 1,
+                expressionProperties: {
+                  'templateOptions.disabled': 'model.monedaId === "PES"',
+                },
+                templateOptions: {
+                  label: 'Cotización',
+                  type: 'number',
+                  required: true,
+                  placeholder: '',
+                },      
+                hideExpression: 'model.monedaId === "PES"',
+              },
+              {
+                key: 'condicionVentaid',
+                type: 'select',
+                className: 'col col-lg-3',
+                templateOptions: {
+                  label: 'Condiciones de Venta',
+                  options: [
+                    { label: 'CONTADO', value: '1' },
+                    { label: 'CTA CTE', value: '2' },
+                  ]
+                },
+              },
+              {
+                key: 'observaciones',
+                type: 'textarea',
+                className: 'col col-lg-6',
+                templateOptions: {
+                  label: 'Observaciones',
+                  required: false,
+                  placeholder:'Información adicional a mostrar en el comprobante'
+                },
+              },
+            ],
+          },
+          { template: '<hr />' },
+          {
+            fieldGroupClassName: 'row',
             fieldGroup: [
               {
-                className: 'col-md-6',
-                type: 'checkbox',
-                key: 'otherToo',
+                key: 'conceptoId',
+                type: 'select',
+                className: 'col col-lg-2',
                 templateOptions: {
-                  label: 'Facturar MiPyme',
+                  label: 'Concepto',
+                  required: TextTrackCue,
+                  options: [
+                    { label: 'PRODUCTOS', value: '1' },
+                    { label: 'SERVICIOS', value: '2' },
+                    { label: 'PRODUCTOS Y SERVICIOS', value: '3' },
+                  ]
+                },
+              },
+              {
+                key: 'fechaDesde',
+                type: 'input',
+                className: 'col col-lg-2',
+                templateOptions: {
+                  label: 'Serv. Desde',
+                  type: 'date',
+                  required: true,
+                },
+                expressionProperties: {
+                  'templateOptions.disabled': 'model.conceptoId === "1"',
+                },
+              },
+              {
+                key: 'fechaHasta',
+                type: 'input',
+                className: 'col col-lg-2',
+                templateOptions: {
+                  label: 'Serv. Hasta',
+                  type: 'date',
+                  required: true,
+                },
+                expressionProperties: {
+                  'templateOptions.disabled': 'model.conceptoId === "1"',
+                },
+              },
+              {
+                key: 'listaPrecioId',
+                type: 'select',
+                className: 'col col-lg-4',
+                templateOptions: {
+                  label: 'Lista de Precios',
+                  required: true,
+                },
+              },
+              {
+                key: 'descuento',
+                type: 'input',
+                className: 'col col-lg-1',
+                templateOptions: {
+                  label: 'Descuento',
+                  required: false,
                 },
               },
             ],
@@ -107,164 +229,200 @@ export class AppComponent {
             fieldGroup: [
               {
                 templateOptions: {
-                  tabTitle: 'Info Comercial',
-                },
-                fieldGroupClassName: 'row',
-                fieldGroup: [
-                  {
-                    key: 'direccion',
-                    type: 'input',
-                    className: 'col-md-5',
-                    templateOptions: {
-                      label: 'Dirección',
-                      required: false,
-                      appearance: 'outline',
-                    },
-                  },
-                  {
-                    key: 'codigoPostal',
-                    type: 'input',
-                    className: 'col-md-1',
-                    templateOptions: {
-                      label: 'C.P.',
-                      required: false,
-                      appearance: 'outline',
-                    },
-                  },
-                  {
-                    key: 'localidad',
-                    type: 'input',
-                    className: 'col-md-3',
-                    templateOptions: {
-                      label: 'Localidad',
-                      required: false,
-                      appearance: 'outline',
-                    },
-                  },
-                  {
-                    key: 'provincia',
-                    type: 'input',
-                    className: 'col-md-3',
-                    templateOptions: {
-                      label: 'Provincia',
-                      required: false,
-                      appearance: 'outline',
-                    },
-                  },
-                  { fieldGroupClassName: 'row' },
-                  {
-                    key: 'mail',
-                    type: 'input',
-                    className: 'col-md-5',
-                    templateOptions: {
-                      label: 'e-mail',
-                      required: false,
-                      type: 'mail',
-                      appearance: 'outline',
-                    },
-                  },
-                  {
-                    key: 'telefono',
-                    type: 'input',
-                    className: 'col-md-3',
-                    templateOptions: {
-                      label: 'Teléfono',
-                      translate: true,
-                      required: false,
-                      maxLength: 120,
-                      appearance: 'outline',
-                    },
-                  },
-                  {
-                    key: 'otros',
-                    type: 'textarea',
-                    className: 'col-md-4',
-                    templateOptions: {
-                      label: 'Otros',
-                      translate: true,
-                      placeholder: 'Comentarios adicionales',
-                      required: false,
-                      appearance: 'outline',
-                    },
-                  },
-                ],
-              },
-              {
-                templateOptions: {
-                  tabTitle: 'Entregas',
-                },
-                expressionProperties: {
-                  'templateOptions.tabDisabled': 'false',
-                  'templateOptions.tabHidden': 'false',
-                },
-                fieldGroup: [
-                  {
-                    key: 'direccionEntrega',
-                    type: 'input',
-                    templateOptions: {
-                      label: 'Dirección de Entrega',
-                      required: false,
-                      appearance: 'outline',
-                    },
-                  },
-                ],
-              },
-              {
-                templateOptions: {
-                  tabTitle: 'Contactos',
-                },
-                expressionProperties: {
-                  'templateOptions.tabDisabled': 'false',
-                  'templateOptions.tabHidden': 'false',
+                  tabTitle: 'Items',
                 },
                 fieldGroup: [
                   {
                     key: 'detalles',
                     type: 'arrayRepeat',
                     templateOptions: {
-                      addText: 'Contactos',
+                      addText: 'Item',
                     },
                     fieldArray: {
                       fieldGroupClassName: 'row',
                       fieldGroup: [
                         {
-                          type: 'input',
-                          className: 'col col-lg-3',
                           key: 'nombre',
+                          type: 'input',
+                          className: 'col col-lg-3', 
                           templateOptions: {
-                            label: 'Nombre',
+                            label: 'Item',
                             required: true,
                           },
                         },
                         {
+                          key: 'descripcion',
                           type: 'input',
-                          className: 'col col-lg-3',
-                          key: 'email',
+                          className: 'col col-lg-3', 
                           templateOptions: {
-                            label: 'e-mail',
-                            type:'email'
+                            label: 'Descripción',
                           },
                         },
                         {
+                          key: 'cantidad',
                           type: 'input',
-                          className: 'col col-lg-2',
-                          key: 'telefono',
+                          className: 'col col-lg-1',
                           templateOptions: {
-                            label: 'Teléfono',
+                            label: 'Cantidad',
+                            type:'number',
+                            required: true,
                           },
                         },
                         {
+                          key: 'precio',
                           type: 'input',
-                          className: 'col col-lg-4',
-                          key: 'direccion',
+                          className: 'col col-lg-1',                       
                           templateOptions: {
-                            label: 'Dirección',
+                            label: 'Precio',
+                            type:'number',
+                            required: true,
+                          },
+                        },
+                        {
+                          key: 'iva',
+                          type: 'input',
+                          className: 'col col-lg-1',                       
+                          templateOptions: {
+                            label: 'IVA',
+                            type:'number',
+                            required: true,
+                          },
+                        },
+                        {
+                          key: 'descuento',
+                          type: 'input',
+                          className: 'col col-lg-1',                       
+                          templateOptions: {
+                            label: 'Descuento',
+                            type:'number',
+                            required: true,
+                          },
+                        },
+                        {
+                          key: 'subtotal',
+                          type: 'input',
+                          className: 'col col-lg-1',                       
+                          templateOptions: {
+                            label: 'Subtotal',
+                            type:'number',
+                            required: true,
                           },
                         },
                       ],
                     },
                   },
                 ],
+                // fieldGroupClassName: 'row',
+                // fieldGroup: [
+                //   {
+                //     key: 'direccion',
+                //     type: 'input',
+                //     className: 'col-md-5',
+                //     templateOptions: {
+                //       label: 'Dirección',
+                //       required: false,
+                //       appearance: 'outline',
+                //     },
+                //   },
+                //   {
+                //     key: 'codigoPostal',
+                //     type: 'input',
+                //     className: 'col-md-1',
+                //     templateOptions: {
+                //       label: 'C.P.',
+                //       required: false,
+                //       appearance: 'outline',
+                //     },
+                //   },
+                //   {
+                //     key: 'localidad',
+                //     type: 'input',
+                //     className: 'col-md-3',
+                //     templateOptions: {
+                //       label: 'Localidad',
+                //       required: false,
+                //       appearance: 'outline',
+                //     },
+                //   },
+                //   {
+                //     key: 'provincia',
+                //     type: 'input',
+                //     className: 'col-md-3',
+                //     templateOptions: {
+                //       label: 'Provincia',
+                //       required: false,
+                //       appearance: 'outline',
+                //     },
+                //   },
+                //   { fieldGroupClassName: 'row' },
+                //   {
+                //     key: 'mail',
+                //     type: 'input',
+                //     className: 'col-md-5',
+                //     templateOptions: {
+                //       label: 'e-mail',
+                //       required: false,
+                //       type: 'mail',
+                //       appearance: 'outline',
+                //     },
+                //   },
+                //   {
+                //     key: 'telefono',
+                //     type: 'input',
+                //     className: 'col-md-3',
+                //     templateOptions: {
+                //       label: 'Teléfono',
+                //       translate: true,
+                //       required: false,
+                //       maxLength: 120,
+                //       appearance: 'outline',
+                //     },
+                //   },
+                //   {
+                //     key: 'otros',
+                //     type: 'textarea',
+                //     className: 'col-md-4',
+                //     templateOptions: {
+                //       label: 'Otros',
+                //       translate: true,
+                //       placeholder: 'Comentarios adicionales',
+                //       required: false,
+                //       appearance: 'outline',
+                //     },
+                //   },
+                // ],
+              },
+              {
+                templateOptions: {
+                  tabTitle: 'Medios de Pago',
+                },
+                expressionProperties: {
+                  'templateOptions.tabDisabled': 'false',
+                  'templateOptions.tabHidden': 'false',
+                },
+                fieldGroup: [
+                  
+                ],
+              },
+              {
+                templateOptions: {
+                  tabTitle: 'Remitos',
+                },
+                expressionProperties: {
+                  'templateOptions.tabDisabled': 'false',
+                  'templateOptions.tabHidden': 'false',
+                },
+   
+              },
+              {
+                templateOptions: {
+                  tabTitle: 'Pagos',
+                },
+                expressionProperties: {
+                  'templateOptions.tabDisabled': 'false',
+                  'templateOptions.tabHidden': 'false',
+                },
+   
               },
             ],
           },
